e170add73fe98d786f0fe1d21ef7b40a03f82009
Osnir Teixeira Costa <osnirteixeiracosta@MacBook-Air-de-Osnir.local>
2019-12-05 02:34:46 -0300

    funcionalidade matricula

--- src/app/controllers/EnrollmentController.js
+++ src/app/controllers/EnrollmentController.js
@@ -1,13 +1,40 @@
 import * as yup from 'yup';
+import { addMonths } from 'date-fns';
 import Enrollment from '../models/Enrollment';
+import Plan from '../models/Plan';
+import Student from '../models/Student';
 
 class EnrollmentController {
   async store(req, res) {
     const schema = yup.object().shape({
       start_date: yup.date().required(),
-      end_date: yup.date().required(),
-      price: yup.date().required(),
+      student_id: yup.number().required(),
+      plan_id: yup.number().required(),
     });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Dados de entrada Invalidos' });
+    }
+
+    const { student_id, plan_id, start_date } = req.body;
+
+    const plan = await Plan.findByPk(plan_id);
+
+    if (!plan) return res.status(400).json({ error: 'Plano nao existe ' });
+
+    const end_date = addMonths(start_date, plan.duration);
+
+    const price = plan.duration * plan.price;
+
+    const enrollment = await Enrollment.create({
+      student_id,
+      plan_id,
+      start_date,
+      end_date,
+      price,
+    });
+
+    return res.json(enrollment);
   }
 }
 
